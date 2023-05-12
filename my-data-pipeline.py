4fecaa5d04680fca0ee966c98abe0aed0bf744cb
Mave li <lidanmei@yahoo.com>
2023-05-12 14:04:45 +1200

    saf

--- my-data-pipeline.py
+++ my-data-pipeline.py
@@ -6,21 +6,25 @@ from datetime import datetime
 import os
 from pytz import timezone
 import process_data
+# connect to s3 bucket
 s3=boto3.resource('s3')
+#the local file path in the local machine
 local_file='/tmp/world_cup.csv'
+# connect to dynamobd
 dynamodb=boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DYNAMO_TABLE'])
 tz_sydney = timezone(os.environ['TZ_LOCAL'])
+# get time stamp to the upload files
 date = datetime.now(tz_sydney).strftime("%Y-%m-%d")
-
 def run(event,context):
+    # display the data structure of objects in event
     print("Stared\t\t" + str(event))
+    # retrieved uploaded objects passed by event
     records = event['Records']
     for record in records:
             bucket = record['s3']['bucket']['name']
             key = record['s3']['object']['key']
             process_data(bucket,key,local_file)
-
 def process_data(bucket,key,file):
     s3get(bucket,key,file)
     with open(file,mode='r') as csv_file:
@@ -41,3 +45,4 @@ def process_data(bucket,key,file):
 def s3get(bucket,key,file):
      s3.object(bucket,key).download_file(file)
 
+ 
